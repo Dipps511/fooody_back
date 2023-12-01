0154f681e8953ff191b061b0f1eb2e8a30b3772a
Dipankar Arora <dipankar.a511@gmail.com>
2023-12-01 12:07:17 -0600

    named stages, moved jest.config.js to root

--- app.test.js
+++ app.test.js
@@ -1,10 +1,18 @@
 const request = require('supertest');
-const app = require('./app'); // Assuming your app is in a file named 'app.js'
+const app = require('./app');
 
 describe('GET /', () => {
   it('responds with JSON containing a welcome message', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
     expect(response.body).toEqual({ mesg: 'Food Ordering - B' });
+  });
+
+  // Add the afterAll hook to close the server
+  afterAll((done) => {
+    // Assuming you have access to the server instance in your app.js file
+    app.close(() => {
+      done();
+    });
   });
 });
